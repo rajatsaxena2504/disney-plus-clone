18b8f91e3b5b613d014ef76f88929657091a46c2
rajatsaxena2504 <83396334+rajatsaxena2504@users.noreply.github.com>
2021-06-09 13:50:41 +0530

    Update firebase.js

--- youtube-disney-clone/src/firebase.js
+++ youtube-disney-clone/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwW7aqc7BUmyoVNs0dwUwkHieT1XvhgIo",
-  authDomain: "disneyplus-clone-a9a03.firebaseapp.com",
-  projectId: "disneyplus-clone-a9a03",
-  storageBucket: "disneyplus-clone-a9a03.appspot.com",
-  messagingSenderId: "660409768503",
-  appId: "1:660409768503:ios:89885a387335c49f79bdb2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
